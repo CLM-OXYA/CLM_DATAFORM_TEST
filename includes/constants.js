f819a1c07f87b65267e839209637bcb9978f3590
Christian Lindholm <clindholm@oxya.com>
2026-02-13 16:26:45 +0000

    constants

---
+++ includes/constants.js
@@ -0,0 +1,3 @@
+ const MULTV = 5; 
+ module.exports = { MULTV };
+ 
--- includes/macros.js
+++
@@ -1,8 +0,0 @@
-function CLMTT(x) {
-  return `${x} * 2`;
-}
-module.exports = { CLMTT };
-
- const MULTV = 5; 
- module.exports = { MULTV };
- 
